57ac302640a2a87f2a99b155326d9171a5c8e5b9
lambbagel <56712744+lambbagel@users.noreply.github.com>
2019-10-18 14:09:53 +0800

    answer

--- t01primeshcflcm_lcm1.py
+++ t01primeshcflcm_lcm1.py
@@ -1,2 +1,7 @@
 # Find the Lowest Common Multiple (LCM) of 18 and 30.
 
+
+
+print("common prime factors of 18 and 30 are 2 and 3; different prime factors are 3 and 5", "find LCM by", "2*3*3*5")
+ans = 2*3*3*5
+print("ans")
